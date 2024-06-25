b68d1b8d40a9a672d7acf4e21151f0dcd00bc178
Huu Hung <hungmnh01@gmail.com>
2024-06-25 23:38:20 +0700

    45

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import reactLogo from './assets/react.svg';
 import { useState } from 'react';
 
 
+
 const App = () => {
   const [todoList, setTodoList] = useState([
     // { id: 1, name: "Learning React" },
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,32 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/register",
+    element: <div>register page</div>,
+  },
+  {
+    path: "/users",
+    element: <div>user page</div>,
+  },
+  {
+    path: "/products",
+    element: <div>product page</div>,
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
