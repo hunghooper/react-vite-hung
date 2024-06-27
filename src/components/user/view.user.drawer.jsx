3961911bb51d92527477c98ae7d581f17f43b7a4
Huu Hung <hungmnh01@gmail.com>
2024-06-27 20:23:12 +0700

    82

--- src/components/user/view.user.drawer.jsx
+++ src/components/user/view.user.drawer.jsx
@@ -21,6 +21,25 @@ const ViewUserDetail = (props) => {
                         <p>Email: {dataDetail.email} </p>
                         <br></br>
                         <p>Phone: {dataDetail.phone} </p>
+                        <br></br>
+                        <div>
+                            <img height={150} width={150}
+                                src={`${import.meta.env.VITE_BACKEND_URL}/images/avatar/${dataDetail.avatar}`} />
+                        </div>
+                        <div>
+                            <label htmlFor='btnUpload'
+                                style={{
+                                    display: "block",
+                                    width: "fit-content",
+                                    marginTop: "15px",
+                                    padding: "5px 10px",
+                                    background: "orange",
+                                    borderRadius: "5px",
+                                    color: "white",
+                                    cursor: "pointer"
+                                }}>Upload Avatar</label>
+                            <input type='file' hidden id='btnUpload'></input>
+                        </div>
                     </>
                     :
                     <>
